89510d7a1da7ba03cb397bcc2005db1397082209
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-09 21:58:21 +0800

    comment out client

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.delete("/addservices/:id",async (req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
